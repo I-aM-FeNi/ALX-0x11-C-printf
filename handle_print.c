b47f698a75710ec52c71a966c64b318b352b9a21
I-aM-FeNi <fenimabusa@gmail.com>
2023-04-23 14:14:21 -0700

    printf

--- handle_print.c
+++ handle_print.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * handle_print - Prints an argument based on its type
@@ -14,7 +14,7 @@ nclude "main.h"
  */
 
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -49,4 +49,3 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	}
 	return (printed_chars);
 }
-
