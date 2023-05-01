3ebad6cbfa30021cd12d94c8e5f8464377302583
Dutchess_Feni <122884364+I-aM-FeNi@users.noreply.github.com>
2023-05-01 18:32:20 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -6,10 +6,11 @@
  *
  * Return: 1 if c is printable, 0 otherwise
  */
+
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
-		return (1);
+	return (1);
 
 	return (0);
 }
@@ -27,7 +28,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-		ascii_code *= -1;
+	ascii_code *= -1;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
@@ -44,10 +45,11 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
+
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
-		return (1);
+	return (1);
 
 	return (0);
 }
@@ -59,12 +61,13 @@ int is_digit(char c)
  *
  * Return: Casted value of num
  */
+
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
-		return (num);
+	return (num);
 	else if (size == S_SHORT)
-		return ((short)num);
+	return ((short)num);
 
 	return ((int)num);
 }
@@ -76,12 +79,13 @@ long int convert_size_number(long int num, int size)
  *
  * Return: Casted value of num
  */
+
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
-		return (num);
+	return (num);
 	else if (size == S_SHORT)
-		return ((unsigned short)num);
+	return ((unsigned short)num);
 
 	return ((unsigned int)num);
 }
